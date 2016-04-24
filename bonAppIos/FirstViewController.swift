7bf88b5025ea2459039714aa4702d20b31c75567
Thomas Macquart <you@example.com>
2016-04-24 18:26:48 +0200

    first commit

---
+++ bonAppIos/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  bonAppIos
+//
+//  Created by Thomas Macquart on 24/04/16.
+//  Copyright (c) 2016 Thomas Macquart. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ bonAppIos/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  bonAppIos
+//
+//  Created by Thomas Macquart on 24/04/16.
+//  Copyright (c) 2016 Thomas Macquart. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
