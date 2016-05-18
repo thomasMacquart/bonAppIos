77d78d83a459e4f3a52ea04e71303c152b7c96e0
Thomas Macquart <you@example.com>
2016-05-18 21:27:16 +0200

    add table view

--- bonAppIos/RecipeListController.swift
+++ bonAppIos/RecipeListController.swift
@@ -8,22 +8,48 @@
 
 import UIKit
 
-class RecipeListController: UIViewController {
+class RecipeListController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var fruits: [String] = []
+    let cellIdentifier = "CellIdentifier"
     
     @IBOutlet weak var createRecipeButton : UIButton!
-
+    @IBOutlet var tableView : UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        fruits = ["Apple", "Pineapple", "Orange", "Blackberry", "Banana", "Pear", "Kiwi", "Strawberry", "Mango", "Walnut", "Apricot", "Tomato", "Almond", "Date", "Melon", "Water Melon", "Lemon", "Coconut", "Fig", "Passionfruit", "Star Fruit", "Clementin", "Citron", "Cherry", "Cranberry"]
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func onCreateRecipeClicked() {
         performSegueWithIdentifier("createRecipeSegue", sender: nil)
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruits.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
+        
+        // Fetch Fruit
+        let fruit = fruits[indexPath.row]
+        
+        // Configure Cell
+        cell.textLabel?.text = fruit
+        
+        return cell
     }
 }
 
